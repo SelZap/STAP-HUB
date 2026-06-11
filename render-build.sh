4158ff288fec55d49bcfb9c35e8a60bc0e33a41c
Crissel Zapatero <crisselzapatero21@gmail.com>
2026-06-11 21:22:50 +0800

    add seeders to build script

--- render-build.sh
+++ render-build.sh
@@ -6,4 +6,7 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
+php artisan db:seed --class=AdminSeeder
+php artisan db:seed --class=StapNodeSeeder
+php artisan db:seed --class=CameraSeeder
 php artisan storage:link
